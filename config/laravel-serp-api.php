599ef31bb0c61cd6ea60093cd5b6af32a4bc88c8
David <kontakt@david-grass-feria.de>
2024-03-27 20:37:57 +0100

    Initial commit

---
+++ config/laravel-serp-api.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+    'serp_api_key'                 => env('SERP_API_KEY',''),
+    
+];
---
+++ src/Providers/AppServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace GrassFeria\LaravelSerpApi\Providers;
+
+use Livewire\Livewire;
+use Illuminate\Support\ServiceProvider;
+use GrassFeria\LaravelSerpApi\Console\Commands\InstallLaravelSerpApiCommand;
+
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+       
+        $this->mergeConfigFrom(
+            __DIR__.'/../../config/laravel-serp-api.php', 'laravel-serp-api'
+        );
+        
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+       
+        //
+        
+        
+
+    }
+}
