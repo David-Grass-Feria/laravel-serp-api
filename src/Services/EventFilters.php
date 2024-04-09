a8016865282f49d30a00954f0da027b39b56c49f
David <kontakt@david-grass-feria.de>
2024-04-09 20:32:14 +0200

    Update EventFilters.php

--- src/Services/EventFilters.php
+++ src/Services/EventFilters.php
@@ -76,7 +76,7 @@ class EventFilters
         $value = str_replace(['März', 'March', 'Mär', 'Mar'], config('app.locale') == 'de' ? 'MÄR' : 'MAR', $value);
         $value = str_replace(['April', 'Apr'], 'APR', $value);
         $value = str_replace(['Mai', 'May'], config('app.locale') == 'de' ? 'MAI' : 'MAY', $value);
-        $value = str_replace(['Juni', 'Jun','June'], 'JUN', $value);
+        $value = str_replace(['Juni', 'Jun','June','JUNe'], 'JUN', $value);
         $value = str_replace(['Juli', 'Jul','July'], 'JUL', $value);
         $value = str_replace(['August', 'Aug'], 'AUG', $value);
         $value = str_replace(['September', 'Sep'], 'SEP', $value);
